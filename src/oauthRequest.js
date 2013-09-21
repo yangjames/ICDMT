667a7055cbb7298781f418569c24bc6f898a4f76
James Yang <james.yang92@gmail.com>
2013-09-20 20:08:50 -0500

    codebird stuff added to git

--- src/oauthRequest.js
+++ src/oauthRequest.js
@@ -1,15 +1,23 @@
-var cb=new Codebird;
+
+$('#testline').html("lol");
+var cb = new Codebird;
+/*
 var current_url=location.toString();
 var query=current_url.match(/\?(.+)$/).split("&amp;");
 var parameters = {};
 var parameter;
 
 cb.setConsumerKey(twitterConsumerKey,twitterConsumerSecret);
-
+*/
+$('#twitter-login-button').click(function() {
+	$('#testline').html("lol");
+    });
+/*
 function requestOAuthToken() {
-    document.getElementById('').value;
+    document.getElementById('testline').value;
     cb.__call(
 	"oauth_requestToken",
 	{oauth_callback: "tbd"},
 	function (reply) {
 	    cb.setToken(reply.
+*/
