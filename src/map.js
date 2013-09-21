f1facba849eef49223777e200aa613712ee4161f
James Yang <james.yang92@gmail.com>
2013-09-21 13:20:57 -0500

    geocoder works

--- src/map.js
+++ src/map.js
@@ -1,4 +1,5 @@
 var map;
+var geocoder;
 
 function mapSetup() {
     google.load('maps','3', {other_params: 'key=' + mapKey + '&sensor=true', callback: initMap});
@@ -6,15 +7,31 @@ function mapSetup() {
 
 function initMap() {
     var mapOptions = {
-	center: new google.maps.LatLng(30.501786,-88.00088),
-	zoom: 11,
+	center: new google.maps.LatLng(25,-90),
+	zoom: 6,
 	mapTypeId: google.maps.MapTypeId.HYBRID
     };
-    
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    geocoder = new google.maps.Geocoder();
     $('#testline').html("set to map canvas");
 }
 
+$('#geocoder-text').bind('keypress',function(e) {
+	if(e.keyCode==13)
+	    geocode($('#geocoder-text').val());
+    });
+
+$('#geocoder-button').click(function() {
+	geocode($('#geocoder-text').val());
+    });
+
+function geocode(address) {
+    geocoder.geocode({'address':address},function(results,status) {
+	    if (status == google.maps.GeocoderStatus.OK)
+		map.setCenter(results[0].geometry.location);
+	});
+}
 
 document.addEventListener('DOMContentLoaded',function() {
 	document.removeEventListener('DOMContentLoaded',arguments.callee,false);
--- src/starter.js
+++ src/starter.js
@@ -1,7 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
     var screenWidth = screen.availWidth;
     var screenHeight = screen.availHeight;
-    var width = 850;
+    var width = 1000;
     var height = 700;
     
     chrome.app.window.create('../html/starter.html', {
