5f26aa8962a568e8366f85d70224e4c1010846d0
James Yang <james.yang92@gmail.com>
2013-09-20 21:59:09 -0500

    OAuth Request submission works

--- src/oauthRequest.js
+++ src/oauthRequest.js
@@ -1,23 +1,57 @@
 
-$('#testline').html("lol");
 var cb = new Codebird;
-/*
 var current_url=location.toString();
-var query=current_url.match(/\?(.+)$/).split("&amp;");
+cb.setConsumerKey(twitterConsumerKey,twitterConsumerSecret);
+
+//var query=current_url.match(/);
+//$('#testline').html(query);
+
+//.split("&amp;");
+
 var parameters = {};
 var parameter;
 
-cb.setConsumerKey(twitterConsumerKey,twitterConsumerSecret);
-*/
+$('#testline').html("clicked");
 $('#twitter-login-button').click(function() {
-	$('#testline').html("lol");
+	$('#testline').html("clicked");
+	cb.__call(
+		  "oauth_requestToken",
+		  {oauth_callback: "oob"},
+		  function (reply) {
+		      // stores it
+		      cb.setToken(reply.oauth_token, reply.oauth_token_secret);
+		      
+		      // gets the authorize screen URL
+		      cb.__call(
+				"oauth_authorize",
+				{},
+				function (auth_url) {
+				    window.codebird_auth = window.open(auth_url);
+				}
+				);
+		  }
+		  );
+
+	$('#twitter-login-button').hide();
+	$('#pin-submission').show();
+	$('#click-confirmation').html('Redirecting to login page. Please type in login information and click "Authorize App." Enter the PIN on the next page in the field below.');
     });
-/*
-function requestOAuthToken() {
-    document.getElementById('testline').value;
-    cb.__call(
-	"oauth_requestToken",
-	{oauth_callback: "tbd"},
-	function (reply) {
-	    cb.setToken(reply.
-*/
+
+$('#pin-submission').submit(function (e) {
+	var $inputs = $('#pin-submission :input');
+	var pin=$inputs.val();
+	$('#testline').html(pin);
+	cb.__call(
+		  "oauth_accessToken",
+		  {oauth_verifier: pin},
+		  function (reply) {
+		      cb.setToken(reply.oauth_token, reply.oauth_token_secret);
+		  }
+		  );
+	
+	cb.__call(
+		  "statuses_update",
+		  {"status": "jquery submission test"},
+		  function(reply) {}
+		  );
+    });
