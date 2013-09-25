ee734eaa3af8ad0da2377193418ae5e087944893
James Yang <james.yang92@gmail.com>
2013-09-25 09:10:07 -0500

    to fix the 404 error

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -2,4 +2,4 @@ var mapKey = "AIzaSyA_h8IKpxr-HLYnIZo9OBYLsvDEMQqzazQ";
 var tableid="13rQeXALrREUCSY9AqDwZ_VYIHO5eFK35VzAKlB4";
 var twitterConsumerKey="V8ggiPTqkbzRsXRv1LmIA";
 var twitterConsumerSecret="NSWNgUCdP771yC4A47NgfgRiEQ7JruFACvebqXhw";
-var serverURL="148.114.64.225";
+var serverURL="http://148.114.64.225";
